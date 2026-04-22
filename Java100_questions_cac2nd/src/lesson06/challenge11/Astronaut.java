a5e618ce419c2fa578ee31990a656d9da6a10937
bisuma <bisuma@C010423070.zipangu.cac.co.jp>
2026-04-22 17:15:38 +0900

    100本ノックLesson06_challenge01-15まで完了

--- Java100_questions_cac2nd/src/lesson06/challenge11/Astronaut.java
+++ Java100_questions_cac2nd/src/lesson06/challenge11/Astronaut.java
@@ -39,35 +39,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 //ここにGammalianクラスを記述する
+class Gammalian {
+	String item;
+	boolean isGood;
 
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		boolean isHit = false;// 数字が見つかったかどうかを判定する
+		for (int i = 0; i < 10; i++) {
+			String target = Integer.toString(i);// ①数字のi(0, 1, 2...)を文字列("0", "1", "2"...)に変換
+			if (item.indexOf(target) != -1) {// 見つかった場合はその位置（0以上）が返り、見つからなかった場合は-1が返ってくる
+				isHit = true;
+				break;// 1つでも数字が見つかればOKなので、残りの数字は探さず終了
+			}
+		}
+		if (isHit) {
+			this.isGood = true;
+		} else {
+			this.isGood = false;
+		}
+
+		this.item = item;
+	}
+
+	public boolean getIsGood() {
+		return isGood;
+	}
+
+	public void setIsGood(boolean isGood) {
+		this.isGood = isGood;
+	}
+}
 
 public class Astronaut {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        System.out.print("γ星人にアイテムを渡してください＞");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String present = br.readLine();
+		System.out.print("γ星人にアイテムを渡してください＞");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String present = br.readLine();
 
+		//ここに適切な処理を記述する
+		Gammalian gammalian = new Gammalian();
+		gammalian.setItem(present);
 
-        //ここに適切な処理を記述する
+		boolean isGood = false;//定義しておかなきゃいけないルール
 
+		//ここに適切な処理を記述する
+		isGood = gammalian.getIsGood();
 
-        boolean isGood = false;
-
-
-        //ここに適切な処理を記述する
-
-
-        System.out.println("\nγ星人：");
-        if(isGood){
-             System.out.println("こんな良いものをもらっていいガンマか！");
-             System.out.println("ゆっくりしていくガンマ。");
-        } else{
-             System.out.println("...ありがとガンマ。");
-             System.out.println("ぶぶ漬けでもいかがガンマか？");
-        }
-    }
+		System.out.println("\nγ星人：");
+		if (isGood) {
+			System.out.println("こんな良いものをもらっていいガンマか！");
+			System.out.println("ゆっくりしていくガンマ。");
+		} else {
+			System.out.println("...ありがとガンマ。");
+			System.out.println("ぶぶ漬けでもいかがガンマか？");
+		}
+	}
 }
--- Java100_questions_cac2nd/src/lesson06/challenge12/Astronaut.java
+++ Java100_questions_cac2nd/src/lesson06/challenge12/Astronaut.java
@@ -32,30 +32,94 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 //ここにOmegalianクラスを記述する
+class Omegalian {
+	String item;
 
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		// ① 末尾の文字とその位置を特定する
+		int lastIndex = item.length() - 1;// 一番最後のインデックス
+		char lastch = item.charAt(lastIndex);// 一番最後の1文字を取り出す
+
+		// ② 数字なら英語の文字列に変換する（privateメソッドを呼び出し)
+		String changeStr = changeLastChar(lastch);
+
+		if (changeStr != null) {
+			// ③ Stringを加工可能なStringBufferに変換する
+			StringBuffer sb = new StringBuffer(item);
+			// replace(開始位置, 終了位置の直前, 置き換える文字)
+			sb.replace(lastIndex, lastIndex + 1, changeStr);
+
+			this.item = new String(sb);// ④加工が終わったので、再びStringに戻して保存する
+		} else {
+			this.item = item;// 数字じゃなかったらそのまま保存
+		}
+	}
+
+	private String changeLastChar(char ch) {
+		String changeStr = null;
+		switch (ch) {
+		case '0':
+			changeStr = "zero";
+			break;
+		case '1':
+			changeStr = "one";
+			break;
+		case '2':
+			changeStr = "two";
+			break;
+		case '3':
+			changeStr = "three";
+			break;
+		case '4':
+			changeStr = "four";
+			break;
+		case '5':
+			changeStr = "five";
+			break;
+		case '6':
+			changeStr = "six";
+			break;
+		case '7':
+			changeStr = "seven";
+			break;
+		case '8':
+			changeStr = "eight";
+			break;
+		case '9':
+			changeStr = "nine";
+			break;
+		default:// 数字以外ならnullのまま
+			break;
+		}
+		return changeStr;
+	}
+
+}
 
 public class Astronaut {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        System.out.print("Ω星人にアイテムを渡してください＞");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String present = br.readLine();
+		System.out.print("Ω星人にアイテムを渡してください＞");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String present = br.readLine();
 
+		//ここに適切な処理を記述する
+		Omegalian omegalian = new Omegalian();
+		omegalian.setItem(present);
 
-        //ここに適切な処理を記述する
+		String item = null;
 
+		//ここに適切な処理を記述する
+		item = omegalian.getItem();
 
-        String item = null;
-
-
-        //ここに適切な処理を記述する
-
-
-        System.out.println("\nΩ星人：");
-        System.out.println("えっ！" + item + "をくれるオメガか！");
-        System.out.println("ありがとうオメガ。");
-    }
+		System.out.println("\nΩ星人：");
+		System.out.println("えっ！" + item + "をくれるオメガか！");
+		System.out.println("ありがとうオメガ。");
+	}
 }
